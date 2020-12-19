d689b6891bf57402ad44202e30bcbd3e6e21bb58
tiltowait <tiltowaitt+github@icloud.com>
2020-12-19 15:51:11 -0800

    Refactor to use discord.ext.commands framework

--- masquerade.py
+++ masquerade.py
@@ -1,7 +1,96 @@
 """Creates and connects an instance of the Tzimisce dicebot."""
 
 import os
+from collections import defaultdict
+
+import discord
+from discord.ext import commands
+
 import tzimisce
 
-CLIENT = tzimisce.Masquerade()
-CLIENT.run(os.environ["TZIMISCE_TOKEN"])
+bot = commands.Bot(command_prefix="/")
+
+# Commands
+
+@bot.command(name="m")
+async def standard_roll(ctx, *args):
+    """Perform a roll without Willpower."""
+    args = " ".join(args)
+    command = defaultdict(lambda: None)
+    command["syntax"] = args
+
+    await tzimisce.Masquerade.handle_command(command, args, ctx)
+
+@bot.command(name="mw")
+async def willpower_roll(ctx, *args):
+    """Perform a roll with Willpower."""
+    args = " ".join(args)
+    command = defaultdict(lambda: None)
+    command["syntax"] = args
+    command["will"] = True
+
+    await tzimisce.Masquerade.handle_command(command, args, ctx)
+
+@bot.command(name="mc")
+async def compact_roll(ctx, *args):
+    """Perform a roll with compact output."""
+    args = " ".join(args)
+    command = defaultdict(lambda: None)
+    command["syntax"] = args
+    command["compact"] = True
+
+    await tzimisce.Masquerade.handle_command(command, args, ctx)
+
+@bot.command(name="mcw")
+async def compact_willpower_roll(ctx, *args):
+    """Perform a Willpower roll with compact output."""
+    args = " ".join(args)
+    command = defaultdict(lambda: None)
+    command["syntax"] = args
+    command["will"] = True
+    command["compact"] = True
+
+    await tzimisce.Masquerade.handle_command(command, args, ctx)
+
+# Events
+
+@bot.event
+async def on_ready():
+    """Print a message letting us know the bot logged in to Discord."""
+    print(f"Logged on as {bot.user}!")
+
+    users = len(bot.users)
+    guilds = len(bot.guilds)
+
+    print(f"Playing on {guilds} servers with {users} users.")
+    print(discord.version_info)
+
+    await bot.change_presence(activity=discord.Game(__status_message()))
+
+@bot.event
+async def on_guild_join(guild):
+    """When joining a guild, log it for statistics purposes."""
+    print(f"Joining {guild}")
+    tzimisce.Masquerade.database.add_guild(guild.id, guild.name)
+    await bot.change_presence(activity=discord.Game(__status_message()))
+
+@bot.event
+async def on_guild_remove(guild):
+    """We don't want to keep track of guilds we no longer belong to."""
+    print(f"Removing {guild}")
+    tzimisce.Masquerade.database.remove_guild(guild.id)
+    await bot.change_presence(activity=discord.Game(__status_message()))
+
+@bot.event
+async def on_guild_update(before, after):
+    """Sometimes guilds are renamed. Fix that."""
+    tzimisce.Masquerade.database.rename_guild(after.id, after.name)
+
+# Misc
+
+def __status_message():
+    """Sets the bot's Discord presence message."""
+    servers = len(bot.guilds)
+    return f"/m help | {servers} chronicles"
+
+bot.run(os.environ["TZIMISCE_TOKEN"])
--- tzimisce/__init__.py
+++ tzimisce/__init__.py
@@ -1,3 +1,3 @@
 """Package tzimisce. The user need only know about Masquerade, the client class."""
 
-from tzimisce.Masquerade import Masquerade
+from tzimisce import Masquerade
