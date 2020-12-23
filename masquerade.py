00f224456da141e211b04da3286905857e351724
tiltowait <tiltowaitt+github@icloud.com>
2020-12-22 20:38:04 -0800

    Add troubleshooting

--- masquerade.py
+++
@@ -1,7 +0,0 @@
-"""Creates and connects an instance of the Tzimisce dicebot."""
-
-import os
-import tzimisce
-
-CLIENT = tzimisce.Masquerade()
-CLIENT.run(os.environ["TZIMISCE_TOKEN"])
--- tzimisce/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Package tzimisce. The user need only know about Masquerade, the client class."""
-
-from tzimisce.Masquerade import Masquerade
